206ccc400132faa7a522bd95af8a96e0e46679a4
tikano <oadmathgenie@gmail.com>
2018-11-11 06:53:26 -0500

    Update application.py

--- application.py
+++ application.py
@@ -39,7 +39,6 @@ class application:
     self.priority = difficulty - 4*dueDate
 
     
-@app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404   
