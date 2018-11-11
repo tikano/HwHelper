c24a0172fda2f11eadde97c6e6dbefa0251ee9b7
tikano <oadmathgenie@gmail.com>
2018-11-11 06:28:34 -0500

    Update application.py

--- application.py
+++ application.py
@@ -240,3 +240,35 @@ e = CAL.events().insert(calendarId = 'primary', sendNotifications = True, body =
 e = CAL.events().insert(calendarId = 'primary', sendNotifications = True, body = EVENT2).execute()
 e = CAL.events().insert(calendarId = 'primary', sendNotifications = True, body = EVENT3).execute()
 e = CAL.events().insert(calendarId = 'primary', sendNotifications = True, body = EVENT4).execute()
+
+import os.path
+import sys
+
+try:
+    import apiai
+except ImportError:
+    sys.path.append(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+    )
+    import apiai
+
+CLIENT_ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
+
+
+def main3():
+    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+
+    request = ai.text_request()
+
+    request.lang = 'de'  # optional, default value equal 'en'
+
+    request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
+
+    request.query = "Hello"
+
+    response = request.getresponse()
+
+    print (response.read())
+  
+if __name__ == '__main__':
+    main3()
