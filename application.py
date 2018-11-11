1bbbcf20f6550b3b029de5497b6733aa97091b4e
tikano <oadmathgenie@gmail.com>
2018-11-11 07:33:01 -0500

    Update application.py

--- application.py
+++ application.py
@@ -67,19 +67,18 @@ def webhook():
     return r
 
 def processRequest(req):
-    if req.get('result').get('action') == 'water-recommendation':
-        result = req.get('result')
-        parameters = result.get('parameters')
-        #print(json.dumps(parameters, indent=4))
-        baseurl = 'https://query.yahooapis.com/v1/public/yql?'
-        yql_query = makeYqlQuery(req)
-        if yql_query is None:
-            return {}
-        yql_url = baseurl + urlencode({'q': yql_query}) + '&format=json'
-        result = urlopen(yql_url).read()
-        data = json.loads(result)
-        #print(data)
-        res = makeWebhookResult(data, parameters)
+    result = req.get('result')
+    parameters = result.get('parameters')
+       #print(json.dumps(parameters, indent=4))
+    baseurl = 'https://query.yahooapis.com/v1/public/yql?'
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + '&format=json'
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+       #print(data)
+    res = makeWebhookResult(data, parameters)
     return res
 
 
